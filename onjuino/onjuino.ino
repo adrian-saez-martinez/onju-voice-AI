d702e1c65b2cd70e38a8df7b65bd7ee82d7e3346
adrian-saez-martinez <adriansaezmartinez@gmail.com>
2024-09-29 21:50:12 +0200

    added OTA updating

--- onjuino/onjuino.ino
+++ onjuino/onjuino.ino
@@ -2,6 +2,7 @@
 #include <WiFiUdp.h>
 #include <driver/i2s.h>
 #include <Adafruit_NeoPixel.h>
+#include <ArduinoOTA.h>
 
 #if __has_include("git_hash.h") // optionally setup post-commit hook to generate git_hash.h
 #include "git_hash.h"
@@ -19,6 +20,10 @@
 // Wi-Fi settings - edit these in credentials.h
 const char *ssid = WIFI_SSID;
 const char *password = WIFI_PASSWORD;
+// OTA settings
+const char *OTApassword = OTA_PASSWORD;
+const char *OTAhostname = "onju-coral-V3";
+
 
 Adafruit_NeoPixel leds(LED_COUNT, LED_PIN, NEO_GRB + NEO_KHZ800);
 
@@ -86,6 +91,8 @@ i2s_pin_config_t pin_config = {
 void setup()
 {
     Serial.begin(115200);
+    while (!Serial);  // Wait for Serial Monitor to open (optional, only needed for some boards)
+    Serial.println("Starting ESP32-S3...");  // Debug message
 
     leds.begin();
     leds.show();
@@ -186,6 +193,31 @@ void setup()
     }
     Serial.println(" Connected to WiFi");
     Serial.println(WiFi.localIP());
+
+    // Initialize OTA
+    ArduinoOTA.setHostname(OTAhostname);
+    ArduinoOTA.setPassword(OTApassword);
+
+    ArduinoOTA.onStart([]() {
+    String type = ArduinoOTA.getCommand() == U_FLASH ? "sketch" : "filesystem";
+    Serial.println("Start updating " + type);
+    });
+    ArduinoOTA.onEnd([]() {
+    Serial.println("\nEnd");
+    });
+    ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
+    Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
+    });
+    ArduinoOTA.onError([](ota_error_t error) {
+    Serial.printf("Error[%u]: ", error);
+    if (error == OTA_AUTH_ERROR) Serial.println("Auth Failed");
+    else if (error == OTA_BEGIN_ERROR) Serial.println("Begin Failed");
+    else if (error == OTA_CONNECT_ERROR) Serial.println("Connect Failed");
+    else if (error == OTA_RECEIVE_ERROR) Serial.println("Receive Failed");
+    else if (error == OTA_END_ERROR) Serial.println("End Failed");
+    });
+    ArduinoOTA.begin();
+    Serial.println("OTA service is ready");
 
     leds.clear();
     leds.show();
@@ -301,6 +333,8 @@ void loop()
             break;
         }
     }
+
+    ArduinoOTA.handle();
 
     WiFiClient client = tcpServer.available();
     if (client)
